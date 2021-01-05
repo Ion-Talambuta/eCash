56b7e520a9ec5afcb732699d7734255865fafdfb
Ion-Talambuta <62880699+Ion-Talambuta@users.noreply.github.com>
2021-01-05 18:37:09 +0200

    Add files via upload

---
+++ Cloudy/src/main/java/com/example/cloud/Cloudy/Controllers/BlogController.java
@@ -0,0 +1,16 @@
+package com.example.cloud.Cloudy.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BlogController {
+
+    @GetMapping("/blog")
+    public String blogMain(Model model){
+        return "blog-main";
+    }
+
+}
+
